894f29e50ebf9d4ba0c13e595f3657bb1ac1e688
Futura_Game <thomas.joriatti@orange.fr>
2025-03-17 10:53:53 +0100

    Juste un test (1)

--- Liste de question avec des proposition.py
+++ Liste de question avec des proposition.py
@@ -1,3 +1,4 @@
+# <--- Partie Thomas --->
 # Spécifiez le chemin du fichier
 fichier_chemin = r"C:\Users\thomas\OneDrive\Documents\Bureau\Repositories\Sujet-QCM-Python\Liste de question avec des proposition.txt"
 
@@ -5,3 +6,16 @@ fichier_chemin = r"C:\Users\thomas\OneDrive\Documents\Bureau\Repositories\Sujet-
 with open(fichier_chemin, 'r', encoding='latin-1') as file:
     contenu = file.read()
     print(contenu)  # Affiche le contenu du fichier pour vérifier
+# <--- Partie Thomas --->
+
+
+
+# <--- Partie Giovanni --->
+
+# <--- Partie Giovanni --->
+
+
+
+# <--- Partie Gautier --->
+
+# <--- Partie Gautier --->
